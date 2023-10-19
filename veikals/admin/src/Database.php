7d2faf437ec818918ae17e8aa1562c99f5702e0b
roberts67HP <robitosins@gmail.com>
2023-10-19 23:49:13 +0300

    Finishing touches for task 9

--- veikals/admin/src/Database.php
+++ veikals/admin/src/Database.php
@@ -1,50 +1,32 @@
 
 <?php
     class Database {
-        // Connection settings
-        private static $dbName;
-        private static $dbHost;
-        private static $dbUsername;
-        private static $dbPassword;
-
-        public static $clientID;
-        public static $clientSecret;
-        public static $redirectUri;
-
         private static $connection = null;
 
-        // returns the database connection object
         public static function getConnection() {
-            // One connection through whole application
             if(self::$connection == null) {
-                $ini_array = parse_ini_file(
-                    $_SERVER['DOCUMENT_ROOT']."/veikals/config/config.ini", 
-                    true
-                );
-                
-                self::$dbName       = $ini_array["database"]["db_name"];
-                self::$dbHost       = $ini_array["database"]["db_host"];
-                self::$dbUsername   = $ini_array["database"]["db_username"];
-                self::$dbPassword   = $ini_array["database"]["db_password"];
-    
-                self::$clientID     = $ini_array["google"]["client_id"];
-                self::$clientSecret = $ini_array["google"]["client_secret"];
-                self::$redirectUri  = $ini_array["google"]["redirect_uri"];
-    
                 try {
+                    require_once 'Config.php';
+
+                    $dbHost = Config::getValue('config', 'database', 'db_host');
+                    $dbName = Config::getValue('config', 'database', 'db_name');
+                    $dbUsername = Config::getValue('config', 'database', 'db_username');
+                    $dbPassword = Config::getValue('config', 'database', 'db_password');
+
                     self::$connection = new PDO(
-                        "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName,
-                        self::$dbUsername, 
-                        self::$dbPassword
+                        "mysql:host=".$dbHost.";"."dbname=".$dbName,
+                        $dbUsername, 
+                        $dbPassword
                     );
                 } catch(PDOException $e) {
                     die($e->getMessage());
                 }
-                // Tell PDO to throw exceptions when errors occur
                 self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             return self::$connection;
         }
+        public static function closeConnection () {
+            self::$connection = null;
+        }
     }
-    Database::getConnection();
 ?>
--- veikals/admin/src/logout.php
+++ veikals/admin/src/logout.php
@@ -1,11 +1,10 @@
 <?php
-    $client = null;
-    $payload = null;
-
-    // remove all session variables
     session_unset();
-    // destroy the session
     session_destroy();
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
 
     header('Location: /veikals/admin/index.php');
+    exit();
 ?>
