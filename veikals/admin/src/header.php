d2ba3f5d8f6bcfa324a4bf75ef4e5db6659626a2
roberts67HP <robitosins@gmail.com>
2023-10-25 10:06:43 +0300

    Fixed logout problem for users/index.php

--- veikals/admin/src/header.php
+++ veikals/admin/src/header.php
@@ -1,7 +1,7 @@
 
 <div id="header">
     <div id="username"><?php echo $_SESSION["name"]; ?></div>
-    <form id="logout" action="logout.php">
+    <form id="logout" action="/veikals/admin/src/logout.php">
         <input type="submit" name="submit" value="Logout" class="btn btn-primary">
     </form>
 </div>
