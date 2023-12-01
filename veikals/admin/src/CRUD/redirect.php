c28baca464fbed7db17511eaf616c37ee899aa42
roberts67HP <robitosins@gmail.com>
2023-12-02 01:32:30 +0200

    Temporary file clear for file renaming problem in github

--- veikals/admin/src/CRUD/redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
-        if (isset($_POST['redirectPath'])) {
-            header('Location: '.$_POST['redirectPath']);
-            exit();
-        }
-    }
-?>
--- veikals/admin/src/order_states/delete.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    $redirectPath = '/veikals/admin/index.php';
-    require_once $_SERVER['DOCUMENT_ROOT'].'/veikals/admin/src/CRUD/deletePage.php';
-?>
