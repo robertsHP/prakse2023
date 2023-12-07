887c1fe4946d6189134d784dcf084ad3a4a6e8d9
roberts67HP <robitosins@gmail.com>
2023-12-07 23:35:12 +0200

    Done task 18

--- veikals/admin/src/api/refreshCall.php
+++ veikals/admin/src/api/refreshCall.php
@@ -1,68 +1,93 @@
 
 <?php
-    include 'apiFunctions.php';
+    require_once 'localFunctions.php';
 
-    refreshDBInfoFor(
-        'clients', 
-        'clients',
+    function refreshDBInfo ($data) {
+        echo "------".$data['local-table-name']."/".$data['api-table-name']."------<br>";
+
+        $apiDataJSON = GET($data['api-table-name']);
+
+        // print_r($apiDataJSON);
+
+        if ($apiDataJSON != null) {
+            saveAndUpdateToLocalDB($data, $apiDataJSON);
+            sendToAPI($data, $apiDataJSON);
+        }
+    }
+
+    refreshDBInfo(
         [
-            'id' => 'client_id',
-            'name' => 'name',
-            'email' => 'email',
-            'phone_number' => 'phone_number',
-            'address' => 'adress'
+            'api-table-name' => 'clients',
+            'local-table-name' => 'clients',
+            'columns' => [
+                'id' => 'client_id',
+                'name' => 'name',
+                'email' => 'email',
+                'phone_number' => 'phone_number',
+                'address' => 'adress'
+            ]
         ]
     );
-    refreshDBInfoFor(
-        'categories', 
-        'product_categories',
+    refreshDBInfo(
         [
-            'id' => 'category_id',
-            'name' => 'name'
+            'api-table-name' => 'categories',
+            'local-table-name' => 'product_categories',
+            'columns' => [
+                'id' => 'category_id',
+                'name' => 'name'
+            ]
         ]
     );
-    refreshDBInfoFor(
-        'order_states', 
-        'order_states',
+    refreshDBInfo(
         [
-            'id' => 'state_id',
-            'name' => 'name'
+            'api-table-name' => 'order_states',
+            'local-table-name' => 'order_states',
+            'columns' => [
+                'id' => 'state_id',
+                'name' => 'name'
+            ]
         ]
     );
-    refreshDBInfoFor(
-        'orders', 
-        'orders',
+    refreshDBInfo(
         [
-            'id' => 'order_id',
-            'number' => 'number',
-            'client_id' => 'client_id',
-            'date' => 'date',
-            'total_sum' => 'total_price',
-            'state_id' => 'state_id'
+            'api-table-name' => 'orders',
+            'local-table-name' => 'orders',
+            'columns' => [
+                'id' => 'order_id',
+                'number' => 'number',
+                'client_id' => 'client_id',
+                'date' => 'date',
+                'total_sum' => 'total_price',
+                'state_id' => 'state_id'
+            ]
         ]
     );
-    refreshDBInfoFor(
-        'products', 
-        'products',
+    refreshDBInfo(
         [
-            'id' => 'product_id',
-            'name' => 'name',
-            'description' => 'description',
-            'image_path' => 'photo_file_loc',
-            'price' => 'price',
-            'count_left' => 'available_amount',
-            'category_id' => 'category_id',
+            'api-table-name' => 'products',
+            'local-table-name' => 'products',
+            'columns' => [
+                'id' => 'product_id',
+                'name' => 'name',
+                'description' => 'description',
+                'image_path' => 'photo_file_loc',
+                'price' => 'price',
+                'count_left' => 'available_amount',
+                'category_id' => 'category_id',
+            ]
         ]
     );
-    refreshDBInfoFor(
-        'order_products',
-        'purchased_goods',
+    refreshDBInfo(
         [
-            'id' => 'purch_goods_id',
-            'order_id' => 'order_id',
-            'product_id' => 'product_id',
-            'count' => 'amount',
-            'total_sum' => 'total_price'
+            'api-table-name' => 'order_products',
+            'local-table-name' => 'purchased_goods',
+            'columns' => [
+                'id' => 'purch_goods_id',
+                'order_id' => 'order_id',
+                'product_id' => 'product_id',
+                'count' => 'amount',
+                'total_sum' => 'total_price'
+            ]
         ]
     );
 ?>
